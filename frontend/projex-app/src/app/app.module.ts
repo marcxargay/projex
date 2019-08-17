c49306a1ced4657aec77344c2b489e8282c83ef0
mxargay <marc.xargay@dxc.com>
2019-08-17 09:04:54 +0200

    init modules core/shared/components

--- frontend/projex-app/src/app/app.module.ts
+++ frontend/projex-app/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    CoreModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
